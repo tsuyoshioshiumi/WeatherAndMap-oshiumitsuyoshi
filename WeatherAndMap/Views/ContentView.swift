cae429f82ded81b7f4d1ef83be7d0343a05dc2ab
oshiumitsuyoshi <ot104752gmail.com>
2025-02-16 16:36:41 +0900

    現在地天気予報完成2

--- WeatherAndMap/Views/ContentView.swift
+++ WeatherAndMap/Views/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
     @StateObject private var weatherVM = WeatherViewModel()
     @StateObject var locationManager = LocationManager()//位置情報管理のオブジェクト
     // 八幡平市大更の緯度・経度
-      var lat: Double = 39.91167
-      var lon: Double = 141.093459
+    var lat: Double = 39.91167
+    var lon: Double = 141.093459
     
     var body: some View {
         NavigationStack {
@@ -32,8 +32,22 @@ struct ContentView: View {
         .padding()
         
         .onAppear {
-                 weatherVM.request3DaysForecast(lat: lat, lon: lon)
-              }
+            
+            getWeatherForecast()
+        }
+    }
+    // 現在地の天気予報取得、順番に処理したいのでDispatchQueue.main.async使用
+    func getWeatherForecast() {
+        DispatchQueue.main.async {
+            if let location = locationManager.location {
+                let latitude = location.coordinate.latitude     // 緯度
+                let longitude = location.coordinate.longitude   // 軽度
+                weatherVM.request3DaysForecast(lat: latitude, lon: longitude) // 天気リクエスト
+                print("LAT:", latitude, "LON:", longitude)
+            } else {
+                print("getting weather is failed")
+            }
+        }
     }
 }
 
